968a722d99ffc72f33091813d91b4f99f2a0c7b5
isulley77 <isulley@stuy.edu>
2019-03-06 15:09:54 -0500

    toString() method

--- KnightBoard.java
+++ KnightBoard.java
@@ -10,6 +10,42 @@ public class KnightBoard{
     
     }
     
+    public String toString(){
+        
+        String sboard = "";
+        
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+            
+            
+            
+                if(board[i][j] == 0){
+                    
+                    sboard += " _ ";
+                }
+            
+                else if(board.length * board[0].length < 10){
+                    
+                    sboard += board[i][j] + " ";
+                }
+                
+                else{
+                    
+                    sboard += " " + board[i][j] + " ";
+                }
+            
+            }
+            
+            sboard += "\n";
+        }
+        
+        
+        return sboard;
+    }
+    
+    
+    
+    
     
     
     
