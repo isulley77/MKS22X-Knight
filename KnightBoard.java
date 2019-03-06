2b3527560bb63e3fd60dc3874cc3e9ee27b69be2
isulley77 <isulley@stuy.edu>
2019-03-06 14:45:43 -0500

    created class and constructor

---
+++ KnightBoard.java
@@ -0,0 +1,36 @@
+public class KnightBoard{
+    
+    
+    private int[][] board;
+    
+    
+    public KnightBoard(int rows, int columns){
+    
+        board = new int[rows][columns];
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args){
+    
+    
+    }
+    
+}
